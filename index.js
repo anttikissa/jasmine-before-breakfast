5844be44d7b415fa30c0fa2dc900ce5ad9d6b8ad
Antti Sykäri <antti.sykari@gmail.com>
2013-10-05 08:48:23 +0300

    Simplistic solution.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 module.exports = {
+
+	// Solve graph dependencies
+	//
+	// Example:
+	//
+	// 'a' depends on 'c', 'c' depends on 'b'
+	// { a: ['b'], b: [], c: ['b'] } -> ['b', 'c', 'a']
 	solve: function(graph) {
-		return [];
+		var order = [];
+		for (node in graph) {
+			order.push(node);
+		}
+		return order;
 	}
 };
